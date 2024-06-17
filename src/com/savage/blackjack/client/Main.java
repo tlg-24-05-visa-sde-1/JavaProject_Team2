a24f4620bb1388b71c928d5ffc1161aaafdf8326
Tomiwa Akinbayo <tomiwaa@tlgcohort.com>
2024-06-17 13:35:01 -0500

    reads .txt file for console welcome

--- src/com/savage/blackjack/client/Main.java
+++ src/com/savage/blackjack/client/Main.java
@@ -1,4 +1,10 @@
 package com.savage.blackjack.client;
 
+import com.savage.blackjack.controller.BlackJackController;
+
 class Main {
+    public static void main(String[] args) {
+        BlackJackController control = new BlackJackController();
+        control.playGame();
+    }
 }
