9f9397bd83633384fc2a2503d9ae095902f88516
Tomiwa Akinbayo <tomiwaa@tlgcohort.com>
2024-06-17 12:02:41 -0500

    Skeleton created

--- src/com/savage/blackjack/Player.java
+++ src/com/savage/blackjack/Player.java
@@ -1,4 +1,25 @@
 package com.savage.blackjack;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Player {
+    private String name;
+    private Integer cardScore;
+    private Map<String, Integer> myCards = new HashMap();
+
+    public Player(){
+    }
+
+    public PlayerResponse responseToDealer(){
+        return PlayerResponse.HIT_ME;
+    }
+
+    public Map<String, Integer> checkCards(){
+        return myCards;
+    }
+
+    public boolean hasBlackJack(){
+        return false;
+    }
 }
--- src/com/savage/blackjack/controller/BlackJackController.java
+++ src/com/savage/blackjack/controller/BlackJackController.java
@@ -1,4 +1,29 @@
 package com.savage.blackjack.controller;
 
+import com.apps.util.Prompter;
+
+import java.util.Scanner;
+
 class BlackJackController {
+    private final Prompter prompter = new Prompter(new Scanner(System.in));
+
+
+    public void playGame() {
+        welcome();
+        playerQuestions();
+        goodBye();
+    }
+
+    public void welcome(){
+
+    }
+
+    public void playerQuestions(){
+
+    }
+
+    public void goodBye(){
+
+    }
+
 }
