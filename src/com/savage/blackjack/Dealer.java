0c39f284f47282bae798d5d78d8edbd4f438c231
Gabriel Viox <GabrielV@tlgcohort.com>
2024-06-18 16:33:15 -0500

    Updated showHands() to print out hasBlackJack, isBust for player and dealer

--- src/com/savage/blackjack/Dealer.java
+++ src/com/savage/blackjack/Dealer.java
@@ -7,21 +7,31 @@ import java.util.Map;
 
 
 public class Dealer extends Player{
+//    private List<Card> deck;
     private String name;
-//    private List<Card> dealerCards = new ArrayList<>();
+    private Hand dealerHand = new Hand();
     private List<Player> players = new ArrayList<>();
-//    private Player player = new Player();
-    private Hand hand = new Hand();
+    private Map<Player, Hand> playerHands = new HashMap<>();
+//    private Hand hand = new Hand();
 
     public Dealer() {
     }
 
     public void shuffleCards(){
-
     }
 
-    public void dealCards(Player player){
-//        hand.addCard();
+    public void addPlayer(String name){
+        Player player = new Player(name);
+        players.add(player);
+        playerHands.put(player, new Hand());
+    }
+
+    public void dealCards(){
+        for(Player player : players){
+            Hand hand = playerHands.get(player);
+            hand.addCards();
+        }
+        dealerHand.addCards();
     }
 
     public List<Integer> giveCard(){
@@ -32,10 +42,32 @@ public class Dealer extends Player{
         return cards;
     }
 
+//    public Card giveNextCard() { // *Dai* - I added this method for the hit or stand
+//        if (deck.isEmpty()) {
+//            throw new IllegalStateException("Deck is empty, cannot deal anymore cards");
+//        }
+//        return deck.remove(0);
+//    }
 
-    public void addPlayer(String name){
-        players.add(new Player(name));
+    // Show hands
+    public void showHands() {
+        for (Player player : players) {
+            System.out.println(player.getName() + "'s hand:");
+            playerHands.get(player).scoreHand();
+            if (playerHands.get(player).hasBlackjack()) {
+                System.out.println(player.getName()+ " Has Blackjack");
+            } else if (playerHands.get(player).handIsBust()) {
+                System.out.println(player.getName() + "is Bust");
+            }
+        }
+        System.out.println("Dealer's hand:");
+        dealerHand.scoreHand();
+        if (dealerHand.hasBlackjack()){
+            System.out.println("Dealer has Blackjack");
+        } else if (dealerHand.handIsBust()) {
+            System.out.println(" Dealer is Bust");}
     }
+
 
     public String toString() {
         return "name= " + name;
