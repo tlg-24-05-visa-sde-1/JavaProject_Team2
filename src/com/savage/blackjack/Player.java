56ed7a99061f68db4d1dec30a8e8e860e0283c7f
Gabriel Viox <GabrielV@tlgcohort.com>
2024-06-18 16:36:18 -0500

    Updated methods and added Daiyrons code. Player can now Inherit methods from Hand. hasBlackJack, isBust, addCards

--- src/com/savage/blackjack/Player.java
+++ src/com/savage/blackjack/Player.java
@@ -1,60 +1,84 @@
 package com.savage.blackjack;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class Player {
 
     private String name;
     private Integer cardScore;
-//    private Hand hand = new Hand();
+    private Hand hand;
 //    public List<Card> myCards = new ArrayList<>();
 
     public Player(){
+        hand = new Hand();
     }
 
     public Player(String name){
         this.name = name;
+        hand = new Hand();
     }
 
     public PlayerResponse responseToDealer(){
         return PlayerResponse.HIT_ME;
-
-
     }
 
-//    public void dealCards(Player player){
-//        hand.addCards();
-//    }
 
     public String getName() {
         return name;
     }
 
-//    public List<Card> checkCards(){
-//        return myCards;
-//    }
+    public List<Card> checkCards(){
+        return hand.getCards();
+    }
 
-/*    public boolean hasBlackJack(){
-        boolean result = false;
-        int sum = 0;
-        for(Card card : myCards){
-            sum += card.getValue();
-        }
-
-        if(sum == BLACK_JACK_NUM){
-            result = true;
-        }
-        return result;
-    }*/
+    public boolean hasBlackJack(){
+        return hand.hasBlackjack();
+    }
 
 //    public void scoreHand(){
-//        for (Card card : myCards){
-//            System.out.println(card.getValue());
-//        }
+//       System.out.println(hand);
+//        // iterate through myCards and show current player hand
+//    }
+
+    public void dealtCard(){
+        hand.addCards();
+    }
+
+    public boolean isBust(){
+        return hand.handIsBust();
+    }
+
+//    public void addCard(Card card){ // *Dai* - I added this method
+//        hand.addCard(card);
+//    }
 //
+//    public void scoreHand() { // *Dai* - I added this method
+//        hand.scoreHand();
+//    }
+//
+//    public void playerTurn(Dealer dealer) { // *Dai* - I added this method
+//        boolean playing = true;
+//        while (playing) {
+//            System.out.printf("%s's hand %s/n", getName(), getCardScore());
+//            System.out.println("Would you like to 'Hit' or 'Stand'?");
+//            String decision = scanner.nextLine().trim().toLowerCase();
+//            switch (decision) {
+//                case "hit":
+//                    addCard(dealer.giveNextCard());
+//                    if (hand.scoreHand() > 21) {
+//                        System.out.printf("You Bust! %s Your Cards Exceed 21.", getName());
+//                        playing = false;
+//                    }
+//                    break;
+//                default:
+//                    System.out.println("Invalid Choice. Please enter 'Hit' or 'Stand'.");
+//                    break;
+//            }
+//        }
+//    }
+
+//    public void scoreHand(){
+//        System.out.println(hand);
 //        // iterate through myCards and show current player hand
 //    }
 
@@ -62,10 +86,8 @@ public class Player {
         return "name= " + name + "cards= ";
     }
 
-//    public void dump(){
-//        for(Card item : myCards){
-//            System.out.println(item.toString());
-//        }
-//    }
+    public void dump(){
+            System.out.println(hand);
+    }
 
 }
